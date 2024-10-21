1fedb26b9d1f14d6071c80f342af9c7700817f77
vikszr <voloshchakvika@gmail.com>
2024-10-21 23:03:00 +0300

    finished homework

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,42 @@
-console.log('form');
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const savedFeedbackForm = localStorage.getItem('feedback-form-state');
+if (savedFeedbackForm) {
+  const parsedFeedbackForm = JSON.parse(savedFeedbackForm);
+
+  formData.email = parsedFeedbackForm.email || '';
+  formData.message = parsedFeedbackForm.message || '';
+  emailInput.value = formData.email;
+  messageInput.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  event.preventDefault();
+  formData.email = event.currentTarget.email.value;
+  formData.message = event.currentTarget.message.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+  form.reset();
+});
